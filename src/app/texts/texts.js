0cd5f8bb3a9db63a9977a4e678043c7488d1abac
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 21:56:21 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -13,8 +13,10 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En Antony Kids, nos especializamos en la confección de chaquetas para niños. Nuestros diseños infantiles están pensados para brindar comodidad y estilo a los más pequeños.",
-        description2: "Cada prenda de Antony Kids combina calidad y creatividad, ofreciendo a los niños la oportunidad de lucir lo último en moda infantil. ¡Descubre nuestros nuevos diseños y vístete de alegría!"
+        description1: "Descripción que se aprobó: Antony Kids se especializa en la confección de chaquetas para niños, combinando funcionalidad y estilo en cada diseño. Desde su fundación, nuestro propósito ha sido ofrecer prendas que no solo protejan a los más pequeños, sino que también reflejen su personalidad y entusiasmo.\n" +
+            "En Antony Kids, estamos dedicados a crear chaquetas con los mejores materiales y un diseño innovador. Nuestro equipo se enfoca en la calidad y el detalle, asegurando que cada chaqueta no solo sea resistente y cómoda, sino también atractiva y moderna.\n" +
+            "Nos enorgullece presentar nuevos diseños infantiles que capturan la imaginación de los niños y la tranquilidad de los padres. Cada prenda está pensada para proporcionar comodidad y estilo, manteniendo a los niños abrigados y felices.\n" +
+            "Si buscas chaquetas que combinen calidad, diseño y funcionalidad para tus pequeños, Antony Kids es tu mejor elección. Descubre nuestra colección y elige entre los más recientes y encantadores diseños para tus hijos. \n"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
